23b81b50fe49695eb5bdffd994b3a9fd1b859235
Winfred Huang <huang.winfred21@gmail.com>
2020-10-28 14:38:10 -0700

    Store JSON content into qna var

--- src/game.js
+++ src/game.js
@@ -1,1 +1,8 @@
 // script file for game screen and game over screen
+let qna = [];
+
+fetch('./public/Apprentice_TandemFor400_Data.json')
+    .then( (res) => res.json())
+    .then( (data) => qna = data);
+
+console.log(qna);
