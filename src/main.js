9a427ff94b9e142a387493b0c2f1f2b59e092c50
Winfred Huang <huang.winfred21@gmail.com>
2020-10-28 17:51:53 -0700

    Add side menubar

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 // script file for landing page and navbar
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
