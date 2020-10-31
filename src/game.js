a66f3dec98acb99cfd93258f07fc0a2d07816754
Winfred Huang <huang.winfred21@gmail.com>
2020-10-30 18:07:54 -0700

    Wrapped game logic into closure to allow private variables

--- src/game.js
+++ src/game.js
@@ -1,9 +1,4 @@
 // script file for game screen and game over screen
-let qna = [];
-let questions = [];
-let idx = 0;
-let score = 0;
-let correctAns = "";
 let landingModal = document.querySelector('.landing-modal');
 let gameScreen = document.querySelector('.game-modal');
 let gameOverScreen = document.querySelector('.game-over-screen');
@@ -13,116 +8,134 @@ let scoreText = document.querySelector('.score');
 let userError = document.querySelector('.warning');
 let checkButton = document.querySelector('.nextOne');
 
+function master() {
+    let qna = [];
+    let questions = [];
+    let idx = 0;
+    let score = 0;
 
-fetch('./public/Apprentice_TandemFor400_Data.json')
-    .then( (res) => res.json())
-    .then( (data) => qna = data);
-
-function shuffleQNA() {
-    let numArray = [];
-    for(let i = 0; i < qna.length; i++) {
-        let num = Math.floor(Math.random() * Math.floor(qna.length));
-        if(numArray.length < 10 && !numArray.includes(num)) numArray.push(num);
+    if(qna.length === 0) {
+        fetch('./public/Apprentice_TandemFor400_Data.json')
+            .then( (res) => res.json())
+            .then( (data) => qna = data);
     }
-    return numArray.map(num => qna[num]);
-}
 
-function shuffleAns(array) {
-    if(array.length !== 4) array.push("Blank Space");
-    array.sort(() => Math.random() - 0.5);
-    return array;
-}
-
-function renderQuestionCard(entry) {
-    let answers = [...entry.incorrect.slice(), entry.correct];
-    let shuffledAnswers = shuffleAns(answers);
-    return {question: entry.question, answers: shuffledAnswers, correct: entry.correct};
-}
-
-function renderAns(ansArray) {
-    for(let i = 0; i < ansArray.length; i++) {
-        document.querySelector(`.c${i+1}`).innerHTML = ansArray[i];
+    function shuffleQNA() {
+        let numArray = [];
+        for(let i = 0; i < qna.length; i++) {
+            let num = Math.floor(Math.random() * Math.floor(qna.length));
+            if(numArray.length < 10 && !numArray.includes(num)) numArray.push(num);
+        }
+        return numArray.map(num => qna[num]);
     }
-}
 
-function startGame() {
-    questions = shuffleQNA();
-    landingModal.style.setProperty('display', 'none');
-    gameOverScreen.style.setProperty('display', 'none');
-    gameScreen.style.setProperty('display', 'flex');
-    handleQuestion();
-}
+    function shuffleAns(array) {
+        if(array.length !== 4) array.push("Blank Space");
+        array.sort(() => Math.random() - 0.5);
+        return array;
+    }
 
-function handleQuestion() {
-    checkButton.disabled = false;
-    clearAllChecks();
-    if(idx >= questions.length) {
-        gameOver();
+    function renderQuestionCard(entry) {
+        let answers = [...entry.incorrect.slice(), entry.correct];
+        let shuffledAnswers = shuffleAns(answers);
+        return {question: entry.question, answers: shuffledAnswers, correct: entry.correct};
     }
-    else {
-        numQuestion.textContent = `Question #${idx+1}`;
-        let questionCard = renderQuestionCard(questions[idx]);
-        questionPrompt.textContent = questionCard.question;
-        renderAns(questionCard.answers);
-        correctAns = questionCard.correct;
-        idx++;
-    }
-}
 
-function handleAnswer() {
-    if(userHasChecked()) {
-        checkButton.disabled = true;
-        let check = checkAnswers();
-        check ? score += 100 : score += 0;
-        setTimeout(() => {
-            handleQuestion();
-        }, 2500);
-    }
-    else {
-        userError.style.setProperty('display', 'block');
-        setTimeout( () => {
-            userError.style.setProperty('display', 'none');
-        }, 1500);
-    }
-}
-
-function userHasChecked() {
-    let value = false;
-    for(let i = 1; i < 5; i++) {
-        if(document.getElementById(`c${i}`).checked === true) {
-            value = true;
+    function renderAns(ansArray) {
+        for(let i = 0; i < ansArray.length; i++) {
+            document.querySelector(`.c${i+1}`).innerHTML = ansArray[i];
         }
     }
-    return value;
-}
 
-function checkAnswers() {
-    let value = false;
-    for(let i = 1; i < 5; i++) {
-        if(document.querySelector(`.c${i}`).innerHTML === correctAns) {
-            document.querySelector(`.cbkg${i}`).style.setProperty('background', 'lightgreen');
+    function handleQuestion() {
+        checkButton.disabled = false;
+        clearAllChecks();
+        if(idx >= questions.length) {
+            gameOver();
+        }
+        else {
+            numQuestion.textContent = `Question #${idx+1}`;
+            let questionCard = renderQuestionCard(questions[idx]);
+            questionPrompt.textContent = questionCard.question;
+            renderAns(questionCard.answers);
+            idx++;
+        }
+    }
+
+    function userHasChecked() {
+        let value = false;
+        for(let i = 1; i < 5; i++) {
             if(document.getElementById(`c${i}`).checked === true) {
                 value = true;
             }
         }
-        else {
-            document.querySelector(`.cbkg${i}`).style.setProperty('background', 'red');
+        return value;
+    }
+
+    function checkAnswers() {
+        let value = false;
+        for(let i = 1; i < 5; i++) {
+            if(document.querySelector(`.c${i}`).innerHTML === questions[idx-1].correct) {
+                document.querySelector(`.cbkg${i}`).style.setProperty('background', 'lightgreen');
+                if(document.getElementById(`c${i}`).checked === true) {
+                    value = true;
+                }
+            }
+            else {
+                document.querySelector(`.cbkg${i}`).style.setProperty('background', 'red');
+            }
+        }
+        return value;
+    }
+
+    function clearAllChecks() {
+        for(let i = 1; i < 5; i++) {
+            document.querySelector(`.cbkg${i}`).style.setProperty('background', 'none');
+            document.getElementById(`c${i}`).checked = false;
         }
     }
-    return value;
-}
 
-function clearAllChecks() {
-    for(let i = 1; i < 5; i++) {
-        document.querySelector(`.cbkg${i}`).style.setProperty('background', 'none');
-        document.getElementById(`c${i}`).checked = false;
+    function gameOver() {
+        gameScreen.style.setProperty('display', 'none');
+        gameOverScreen.style.setProperty('display', 'flex');
+        scoreText.innerHTML = `${score} out of 1000`;
+        reset();
+    }
+    
+    function reset() {
+        idx = 0;
+        score = 0;
+    }
+
+    return {
+        startGame: function() {
+            questions = shuffleQNA();
+            landingModal.style.setProperty('display', 'none');
+            gameOverScreen.style.setProperty('display', 'none');
+            gameScreen.style.setProperty('display', 'flex');
+            handleQuestion();
+        },
+        handleAnswer: function() {
+            if(userHasChecked()) {
+                checkButton.disabled = true;
+                let check = checkAnswers();
+                check ? score += 100 : score += 0;
+                setTimeout(() => {
+                    handleQuestion();
+                }, 2500);
+            }
+            else {
+                userError.style.setProperty('display', 'block');
+                setTimeout( () => {
+                    userError.style.setProperty('display', 'none');
+                }, 1500);
+            }
+        },
+        defaultReset: function() {
+            idx = 0;
+            score = 0;
+        }
     }
 }
 
-function gameOver() {
-    gameScreen.style.setProperty('display', 'none');
-    gameOverScreen.style.setProperty('display', 'flex');
-    scoreText.innerHTML = `${score} out of 1000`;
-    idx = 0;
-    score = 0;
-}
+var control = master();
--- src/main.js
+++ src/main.js
@@ -12,6 +12,14 @@ function refresh() {
     landingModal.style.setProperty('display', 'flex');
     gameOverScreen.style.setProperty('display', 'none');
     gameScreen.style.setProperty('display', 'none');
-    idx = 0;
-    score = 0;
+    control.defaultReset();
+}
+
+function masterControl(value) {
+    if(value === 0) {
+        control.startGame();
+    }
+    else {
+        control.handleAnswer();
+    }
 }
